ebe1e812120d0db6a17835d01b0b95397be579ff
shenfangwu <a15524318802@163.com>
2024-05-09 13:29:42 +0800

    完成1-3例子

---
+++ CppPrimer/1_Start/1-3/1-3.cpp
@@ -0,0 +1,14 @@
+﻿// 1-3.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "/*"<<std::endl;
+    std::cout << "*/" << std::endl;
+    // 错误：界定符不可以嵌套
+    std::cout << /* "*/" */;
+    // 错误：界定符不可以嵌套
+    std::cout << /* "*/" /* "*/" */;
+}
