8c1eaabce39b4e16096d34b7f26cad3021517274
shenfangwu <a15524318802@163.com>
2024-05-13 14:17:51 +0800

    完成2-3复合类型

---
+++ CppPrimer/2_Variables and Basic Types/2-3复合类型/2-3复合类型.cpp
@@ -0,0 +1,35 @@
+﻿// 2-4复合类型
+//
+
+#include <iostream>
+// 练习2.16
+#if 0
+int main()
+{
+    int i = 0, & r1 = i;
+    double d = 0, & r2 = d;
+    r2 = 3.1415926356;
+    r2 = r1;
+    i = r2;
+    r1 = d;
+    return 0;
+}
+#endif
+// 练习2.17
+#if 0
+int main()
+{
+    int i = 0, & ri = i;
+    i = 5; ri = 10;
+    std::cout << i << " " << ri << std::endl;
+    return 0;
+}
+#endif
+#if 1 
+int main() {
+    int i = 0;
+    // double* dp = &i;int*不可以初始化double*
+    // int* ip = i;int不可以初始换int*
+    int* p = &i;
+}
+#endif
