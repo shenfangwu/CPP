6a89bc20b0ee529c871a2648a5a1e60128041c34
shenfangwu <a15524318802@163.com>
2024-05-15 13:26:53 +0800

    完成3-1命名空间using声明

---
+++ CppPrimer/3_Strings Vectors and Arrays/3-1/3-1.cpp
@@ -0,0 +1,14 @@
+﻿// 3-1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+// 1 通过下列using声明，我们可以使用标准库中的名字
+// 2 每个名字都要独立的using声明
+// 3 头文件一般不应包含using声明
+using std::cout;
+using std::endl;
+int main()
+{
+    cout << "Hello World!\n" << endl;;
+}
+
