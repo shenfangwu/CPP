da42d0c1f9a7e815e515116a96dc05aa1333566f
shenfangwu <a15524318802@163.com>
2024-05-29 10:41:45 +0800

    完成4-1基础

---
+++ CppPrimer/4_Expression/4-1/4-1.cpp
@@ -0,0 +1,18 @@
+﻿// 4-1.cpp
+#include <iostream>
+using namespace std;
+
+int func1() {
+    return 1;
+}
+int func2() {
+    return 2;
+}
+int main()
+{
+    // C++大部分运算符没有明确求值顺序
+    int i = 0;
+    cout << i <<" " << i++ << endl;// 不明确是先执行i++还是i
+    cout <<func1()*func2()<< endl;// 不明确先执行func1还是func2
+}
+
