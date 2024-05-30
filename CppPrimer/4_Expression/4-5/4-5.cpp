a5aa6e61d559f0418cd12149013b3a0fda834d53
shenfangwu <a15524318802@163.com>
2024-05-30 10:08:38 +0800

    完成4-5递增和递减运算符

---
+++ CppPrimer/4_Expression/4-5/4-5.cpp
@@ -0,0 +1,23 @@
+﻿// 4-5.递增和递减运算符
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // 练习4-18
+    vector<int> v = { 1,2,3,4,5 };
+    auto pbeg = v.begin();
+    while (pbeg != v.end() && *pbeg >= 0) {
+        cout << *++pbeg << endl;
+    }
+    // 练习4-19
+    int* ptr = nullptr;
+    int ival = 0;
+    vector<int> vec{ 1,2,3,4,5 };
+    ptr != 0 && *ptr++;// ptr如果不是空指针且ptr指向的内容也不为空，向后移动指针
+    ival++ && ival;
+    vec[ival++] <= vec[ival];
+}
