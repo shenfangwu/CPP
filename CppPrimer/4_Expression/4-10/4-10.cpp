f9c48fca8e73d760c6576c33c716ce04ffb7e7b2
shenfangwu <a15524318802@163.com>
2024-05-31 14:54:38 +0800

    完成4-10逗号运算符

---
+++ CppPrimer/4_Expression/4-10/4-10.cpp
@@ -0,0 +1,13 @@
+﻿// 4-10 逗号运算符
+
+#include <iostream>
+
+int main()
+{
+    // 4-33
+    int someValue = -1;
+    int x = 0;
+    int y = 0;
+    std::cout<<someValue ? ++x, ++y : --x, --y;// -1
+}
+
