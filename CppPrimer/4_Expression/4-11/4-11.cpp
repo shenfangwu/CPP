c24991cacc3d7294f868b7e13c624323ae083671
shenfangwu <a15524318802@163.com>
2024-05-31 16:31:29 +0800

    完成4-11类型转换

---
+++ CppPrimer/4_Expression/4-11/4-11.cpp
@@ -0,0 +1,22 @@
+﻿// 4-11类型转换
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+   // 练习4-36
+	int i = 1;
+	double d = 3.0;
+	i *= static_cast<int>(d);
+	// 练习4-37
+	double d;
+	const string* ps;
+	char* pc;
+	void* pv;
+	pv = static_cast<void*>(const_cast<string*>(ps));
+	i = static_cast<int>(*pc);
+	pv = static_cast<void*>(&d);
+	pc = static_cast<char*>(pv);
+}
+
