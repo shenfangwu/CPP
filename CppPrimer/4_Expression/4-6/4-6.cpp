16c9142e79d5c7e489d7daf98f9030e4db5032c1
shenfangwu <a15524318802@163.com>
2024-05-31 10:33:54 +0800

    完成4-6成员访问运算符

---
+++ CppPrimer/4_Expression/4-6/4-6.cpp
@@ -0,0 +1,16 @@
+﻿// 4-6.cpp
+#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    // 练习4-20
+    vector<string> vec = { "wfs","18704028051" };
+    vector<string>::iterator iter = vec.begin();
+    *iter++;
+    (*iter)++;// string没++运算符
+    *iter.empty();// vector<string>::iterator没empty()函数
+    iter->empty();//等价于(*iter).empty()
+    ++*iter;// string没++运算符
+    iter++->empty();
+}
