5e7db3bd0476cbe93f54b85e6983d0011a9a901e
shenfangwu <a15524318802@163.com>
2024-05-31 13:46:44 +0800

    完成4-8位运算符

---
+++ CppPrimer/4_Expression/4-8/4-8.cpp
@@ -0,0 +1,19 @@
+﻿// 4-8位运算符
+
+#include <iostream>
+
+int main()
+{
+    // 练习4.25
+    char c = 'q';
+    c = ~c << 6;
+    std::cout << c << std::endl;
+    // 练习4.27
+    unsigned long ul1 = 3, ul2 = 7;
+    ul1 & ul2;// 位与操作  00000000000000000000000000000011 (ul1)&00000000000000000000000000000111 (ul2)
+    ul1 | ul2;// 位或操作
+    ul1 && ul2;// 逻辑与
+    ul1 || ul2;
+    return 0;
+}
+
