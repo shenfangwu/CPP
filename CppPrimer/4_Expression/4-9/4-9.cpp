092cf068875497f73a870e1632caf86141467c58
shenfangwu <a15524318802@163.com>
2024-05-31 14:22:33 +0800

    完成4-9 sizeof运算符

---
+++ CppPrimer/4_Expression/4-9/4-9.cpp
@@ -0,0 +1,27 @@
+﻿// 4-9
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 练习4.28
+	cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl << endl;
+	cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
+	cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
+	cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl;
+	cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl << endl;
+
+	cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
+	cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
+	cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
+	cout << "long long:\t" << sizeof(long long) << " bytes" << endl << endl;
+
+	cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
+	cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
+	cout << "long double:\t" << sizeof(long double) << " bytes" << endl << endl;
+	return 0;
+	// 练习4-29
+	int x[10];   int* p = x;
+	cout << sizeof(x) / sizeof(*x) << endl;// 10
+	cout << sizeof(p) / sizeof(*p) << endl;// 2
+}
