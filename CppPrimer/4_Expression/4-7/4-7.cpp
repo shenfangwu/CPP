4a646a62a5792763625b0564ff53e7e7bd444405
shenfangwu <a15524318802@163.com>
2024-05-31 11:08:40 +0800

    完成4-7条件运算符

---
+++ CppPrimer/4_Expression/4-7/4-7.cpp
@@ -0,0 +1,23 @@
+﻿// 4-7条件运算符
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    // 练习4-21
+    vector<int> vec{ 1,2,3,4,5 };
+    auto lt = vec.begin();
+    while (lt != vec.end()) {
+        /*if (*lt % 2) {
+            *lt = *lt + *lt;
+        }*/
+        *lt = *lt % 2 ? *lt * 2 : *lt;// 使用条件运算符代替if语句
+        ++lt;
+    }
+    // 练习4-23
+    string s = "word";
+    string p1 = s + (s[s.size() - 1] == 's' ? "" : "s");// 条件运算符的优先级低、且满足右结合律
+    return 0;
+}
+
