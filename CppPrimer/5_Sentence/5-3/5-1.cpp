a3e463a1b81fc96d5438efdc739db375fba8d705
shenfangwu <a15524318802@163.com>
2024-06-03 15:37:35 +0800

    完成5-1简单语句

---
+++ CppPrimer/5_Sentence/5-3/5-1.cpp
@@ -0,0 +1,14 @@
+﻿// 5-1简单语句
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // 练习5-3
+    int  sum = 0, val = 1;
+    while ((sum += val,val++),val <= 10) {// 使用逗号表达式完成while的逻辑
+     }
+    cout << "Sum of 1 to 10 inclusive is " << sum << endl;
+    return 0;
+}
