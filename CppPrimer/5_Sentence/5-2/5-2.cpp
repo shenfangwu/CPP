f971d929ea2e3279c920bb3081523d1ee6de9acf
shenfangwu <a15524318802@163.com>
2024-06-03 16:19:05 +0800

    完成5-2语句作用域

---
+++ CppPrimer/5_Sentence/5-2/5-2.cpp
@@ -0,0 +1,16 @@
+﻿// 5-2语句作用域
+
+#include <iostream>
+int get_num() {
+    return 1;
+}
+int main()
+{
+    while (int i = get_num()) {
+        std::cout << i << std::endl;
+    }
+    i = 0;// 循环外不可以访问i
+
+}
+
+
